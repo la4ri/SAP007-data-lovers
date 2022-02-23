fd12b65f469ad027c363cf31b73c3f1c0cd6b3b5
flavia-dantas <flavia.dantas2@hotmail.com>
2022-02-22 21:44:15 -0300

    filtro-ordemAZ

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { filtroEspecie, filtroGenero, filtroStatus } from './data.js';
+import { filtroEspecie, filtroGenero, filtroStatus, filtroOrdem } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 const listaPersonagens = data.results;
 const filtroSelecionadoEspecie = document.getElementById("filtro-especies");
 const filtroSelecionadoGenero = document.getElementById("filtro-genero");
 const filtroSelecionadoStatus = document.getElementById("filtro-status");
+const filtroSelecionadoOrdem = document.getElementById("filtro-ordem");
+const botaoLimpar= document.getElementById("bnt-limpar");
 
 function mostrarCards(data) { // innerHTML para mostrar os cards na pagina personagens(html)
   document.getElementById("mostra-cards").innerHTML = data.map((item) => `
@@ -26,26 +28,14 @@ function mostrarCards(data) { // innerHTML para mostrar os cards na pagina perso
     .join("");
 
 }
-mostrarCards(data.results);
-//console.log(data);
-
-//O método map() executa uma função em todos os itens de um array e retorna um novo array após a manipulação,
-// ou seja, não sobrescreve o array original.
-// " => " é chamado de arrow function, que é uma função/callback que possui uma sintaxe + curta, comparada com uma função.
-// o "item" foi um parametro criado para puxar os dados do array (array com as informações dos personagens, nome,img etc)
-// essa crase "`" é chamada de template string . Template Strings são strings que permitem expressões embutidas.
-// Pode utilizar string multi-linhas e interpolação de string com elas.
-//O método join() junta todos os elementos de um array em uma string e retorna esta string.
-//o padrão do array é separado por vírgula, se tirar o join, os dados dos personagens serão separados por virgulas.
-//colocamos as aspas ("") pois não queremos que os dados fiquem separados por virgula
+mostrarCards(listaPersonagens);
 
 function filtrarEspecie() {
   const valorSelecionadoEspecie = filtroSelecionadoEspecie.value;
   const selecionadoEspecie = filtroEspecie(listaPersonagens, valorSelecionadoEspecie);
   mostrarCards(selecionadoEspecie);
 }
-filtroSelecionadoEspecie.addEventListener("change", filtrarEspecie);
-
+filtroSelecionadoEspecie.addEventListener('change', filtrarEspecie);
 
 function filtrarGenero(){
   const valorSelecionadoGenero = filtroSelecionadoGenero.value;
@@ -60,3 +50,25 @@ function filtrarStatus(){
   mostrarCards(selecionadoStatus);
 }
 filtroSelecionadoStatus.addEventListener('change', filtrarStatus);
+
+function filtrarOrder() {
+  const valorSelecionadoOrdem = filtroSelecionadoOrdem.value;
+  const selecionadoOrdem = filtroOrdem(listaPersonagens,valorSelecionadoOrdem);
+  mostrarCards(selecionadoOrdem);
+}
+filtroSelecionadoOrdem.addEventListener('change',filtrarOrder);
+
+function limparFiltros(){
+  window.location.reload(); //recarrega a página
+  }
+  botaoLimpar.addEventListener("click",limparFiltros);
+
+//O método map() executa uma função em todos os itens de um array e retorna um novo array após a manipulação,
+// ou seja, não sobrescreve o array original.
+// " => " é chamado de arrow function, que é uma função/callback que possui uma sintaxe + curta, comparada com uma função.
+// o "item" foi um parametro criado para puxar os dados do array (array com as informações dos personagens, nome,img etc)
+// essa crase "`" é chamada de template string . Template Strings são strings que permitem expressões embutidas.
+// Pode utilizar string multi-linhas e interpolação de string com elas.
+//O método join() junta todos os elementos de um array em uma string e retorna esta string.
+//o padrão do array é separado por vírgula, se tirar o join, os dados dos personagens serão separados por virgulas.
+//colocamos as aspas ("") pois não queremos que os dados fiquem separados por virgula
