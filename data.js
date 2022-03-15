453f295b8d24d3916c0af3f5fe56f5c2c14a38ba
flavia-dantas <flavia.dantas2@hotmail.com>
2022-03-14 21:38:46 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,39 @@
+export const filtroEspecie = (listaPersonagens, especie) => {
+  const resultadoEspecie = listaPersonagens.filter((personagem) => personagem.species.toLowerCase() === especie.toLowerCase());
+  return resultadoEspecie;
+};
+
+export const filtroGenero = (listaPersonagens, genero) => {
+  const resultadoGenero = listaPersonagens.filter((personagem) => personagem.gender.toLowerCase() === genero.toLowerCase());
+  return resultadoGenero;
+};
+
+export const filtroStatus = (listaPersonagens, status) => {
+  const resultadoStatus = listaPersonagens.filter((personagem) => personagem.status.toLowerCase() === status.toLowerCase());
+  return resultadoStatus;
+};
+
+export const filtroOrdem = (listaPersonagens, ordem) => {
+  if (ordem === "az") {
+    return listaPersonagens.sort((a, b) => a.name > b.name ? 1 : -1)
+  } else {
+    return listaPersonagens.sort((a, b) => a.name > b.name ? -1 : 1)
+  }
+};
+
+export const filtroPesquisa = (listaPersonagens, acharValor) => {
+  const resultadoPesquisa = listaPersonagens.filter(personagem => personagem.name.toLowerCase().includes(acharValor.toLowerCase()));
+  return resultadoPesquisa;
+};
+
+export const calculaFiltros = (listaPersonagens,filtroSelecionado)=>{
+  const resultadoCalculo = parseFloat((filtroSelecionado.length * 100) / listaPersonagens.length).toFixed(2);
+  return resultadoCalculo;
+};
+
+// toLowerCase() retorna o valor da string que foi chamada convertido para minúsculo
+// Ex: A especie, gernero e status a primeira letra é maiuscula
+// O método sort() ordena os elementos do próprio array e retorna o array.
+// operador ternário - funciona com o mesmo conceito do if e else
+// A sintaxe é: {condição} ? {expressão se verdadeiro} : {expressão se falso} s
+// condição ? a : b
---
+++ main.js
@@ -0,0 +1,121 @@
+import { filtroEspecie, filtroGenero, filtroStatus, filtroOrdem, filtroPesquisa, calculaFiltros } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+const listaPersonagens = data.results;
+const filtroSelecionadoEspecie = document.getElementById("filtro-especies");
+const filtroSelecionadoGenero = document.getElementById("filtro-genero");
+const filtroSelecionadoStatus = document.getElementById("filtro-status");
+const filtroSelecionadoOrdem = document.getElementById("filtro-ordem");
+const calculoFiltros = document.getElementById("calculo-filtro");
+const filtroBusca = document.getElementById("pesquisa");
+const botaoLimpar = document.getElementById("bnt-limpar");
+
+function criarCard (personagem) {
+  const divCard = document.createElement('div')
+  divCard.classList.add('cards')
+  divCard.innerHTML =  `
+    <div class="frente-card">
+      <img class="img-card" src="${personagem.image}"  alt="cards">
+      <p class="nome-personagem">${personagem.name}</p>
+    </div>
+    <div class="verso-card">
+      <p><b>Gênero:</b> ${personagem.gender}</p>
+      <p><b>Status:</b> ${personagem.status}</p>
+      <p><b>Espécie:</b> ${personagem.species}</p>
+      <p><b>Origem:</b> ${personagem.origin.name}</p>
+      <p><b>Localização:</b> ${personagem.location.name}</p>
+      <p><b>Aparecem em: </b> ${personagem.episode.length} episódios</p>
+    </div>
+  `
+
+  function chameiClick() {
+    divCard.classList.toggle('mostra-verso');
+  }
+
+  divCard.addEventListener('click', chameiClick)
+  return divCard
+}
+
+
+
+function mostrarCards(data) { // innerHTML para mostrar os cards na pagina personagens(html)
+  const conteiner = document.getElementById("mostra-cards");
+  conteiner.innerHTML = "";
+  data.map(personagem => {
+    const div = criarCard(personagem)
+    conteiner.appendChild(div)
+  })
+
+}
+mostrarCards(listaPersonagens);
+
+
+
+function calcularFiltros(listaPersonagens, filtroSelecionado) {
+  calculoFiltros.innerHTML = "",
+  calculoFiltros.style.display = "block";
+  const calculoResultado = calculaFiltros(listaPersonagens, filtroSelecionado);
+  calculoFiltros.innerHTML = `Existem ${filtroSelecionado.length} personagens deste filtro, e
+  representam ${calculoResultado}% do total dos personagens`
+}
+
+function limparFiltros() {
+  window.location.reload(); //recarrega a página
+}
+botaoLimpar.addEventListener("click", limparFiltros);
+
+function filtrarEspecie() {
+  const valorSelecionadoEspecie = filtroSelecionadoEspecie.value;
+  const selecionadoEspecie = filtroEspecie(listaPersonagens, valorSelecionadoEspecie);
+  mostrarCards(selecionadoEspecie);
+  calcularFiltros(listaPersonagens,selecionadoEspecie);
+}
+filtroSelecionadoEspecie.addEventListener('change', filtrarEspecie);
+
+function filtrarGenero() {
+  const valorSelecionadoGenero = filtroSelecionadoGenero.value;
+  const selecionadoGenero = filtroGenero(listaPersonagens, valorSelecionadoGenero);
+  mostrarCards(selecionadoGenero);
+  calcularFiltros(listaPersonagens,selecionadoGenero);
+}
+filtroSelecionadoGenero.addEventListener('change', filtrarGenero);
+
+function filtrarStatus() {
+  const valorSelecionadoStatus = filtroSelecionadoStatus.value;
+  const selecionadoStatus = filtroStatus(listaPersonagens, valorSelecionadoStatus);
+  mostrarCards(selecionadoStatus);
+  calcularFiltros(listaPersonagens,selecionadoStatus);
+}
+filtroSelecionadoStatus.addEventListener('change', filtrarStatus);
+
+function filtrarOrder() {
+  const valorSelecionadoOrdem = filtroSelecionadoOrdem.value;
+  const selecionadoOrdem = filtroOrdem(listaPersonagens, valorSelecionadoOrdem);
+  mostrarCards(selecionadoOrdem);
+}
+filtroSelecionadoOrdem.addEventListener('change', filtrarOrder);
+
+function filtrarPesquisa() {
+  const valorSelecionadoPesquisa = filtroBusca.value;
+  const selecionarPesquisa = filtroPesquisa(listaPersonagens, valorSelecionadoPesquisa);
+  mostrarCards(selecionarPesquisa);
+}
+filtroBusca.addEventListener("keyup", filtrarPesquisa);
+
+
+
+
+
+
+
+
+
+//O método map() executa uma função em todos os itens de um array e retorna um novo array após a manipulação,
+// ou seja, não sobrescreve o array original.
+// " => " é chamado de arrow function, que é uma função/callback que possui uma sintaxe + curta, comparada com uma função.
+// o "item" foi um parametro criado para puxar os dados do array (array com as informações dos personagens, nome,img etc)
+// essa crase "`" é chamada de template string . Template Strings são strings que permitem expressões embutidas.
+// Pode utilizar string multi-linhas e interpolação de string com elas.
+//O método join() junta todos os elementos de um array em uma string e retorna esta string.
+//o padrão do array é separado por vírgula, se tirar o join, os dados dos personagens serão separados por virgulas.
+//colocamos as aspas ("") pois não queremos que os dados fiquem separados por virgula
